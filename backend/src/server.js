98097004cc2f5c7760416c3440585a402b39844c
karim <hassansaid2528@gmail.com>
2026-05-17 22:44:37 +0300

    added deployment option

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
+
+const __dirname =  path.resolve();
 dotenv.config();
 const app = express();
 
+
 // Middleware
 app.use(express.json());
+
 // Routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT;
 
+// Serve static files from the React app in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use( (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}` );
 });
