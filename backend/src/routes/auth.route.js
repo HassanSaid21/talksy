520fc58e6b01b6c4a33f17b0b29dc182610c7bd2
karim <hassansaid2528@gmail.com>
2026-06-12 15:25:00 +0300

    add cloudinary integration and update profile picture functionality

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,7 +4,10 @@ import {
   rotateRefreshToken,
   signup,
   logout ,
+  updateProfile
 } from "../controllers/auth.controller.js";
+
+import { protect } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 // @route   GET api/auth/login
@@ -22,5 +25,11 @@ router.post("/signup", signup);
 router.post("/refresh-token", rotateRefreshToken);
 
 // @route   POST api/auth/logout
+
+router.put('/update-profile',  protect, updateProfile);
+
+
+
+
 
 export default router;
